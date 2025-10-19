0d196d3fcf3a53de7d6da345022c1bcca576f769
siddheshtodkar <stodkar53@gmail.com>
2025-10-19 20:38:57 +0530

    Edit idea

--- src/app/components/idea-form/idea-form.component.ts
+++ src/app/components/idea-form/idea-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormArray, Validators, FormsModule, ReactiveFormsModule, NonNullableFormBuilder } from '@angular/forms';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { IdeaService } from '../../services/idea.service';
 
 @Component({
@@ -11,11 +11,13 @@ import { IdeaService } from '../../services/idea.service';
 })
 export class IdeaFormComponent {
   route = inject(ActivatedRoute)
+  router = inject(Router)
   formBuilder = inject(NonNullableFormBuilder)
   ideaService = inject(IdeaService)
   editFlag = signal<boolean>(false)
   isPending = signal<boolean>(false)
   ideaId = this.route.snapshot.paramMap.get('id')
+  idea$ = this.ideaService.fetchIdea(this.ideaId || '')
   ideaForm = this.formBuilder.group({
     title: ['', Validators.required],
     summary: ['', Validators.required],
@@ -33,15 +35,35 @@ export class IdeaFormComponent {
   }
   submit() {
     console.log(this.ideaForm.getRawValue())
-    if (this.ideaForm.valid)
-      this.ideaService.createIdea(this.ideaForm.getRawValue()).subscribe(data => {
-        console.log(data);
+    if (this.ideaForm.valid) {
+      if (this.editFlag()) {
+        this.ideaService.updateIdea(this.ideaId!, this.ideaForm.getRawValue()).subscribe(data => {
+          console.log(data);
+          this.router.navigateByUrl('/ideas')
+        }, err => {
+          console.log(err);
+        })
+      }
+      else {
+        this.ideaService.createIdea(this.ideaForm.getRawValue()).subscribe(data => {
+          console.log(data);
+          this.router.navigateByUrl('/ideas')
+        }, err => {
+          console.log(err);
+        })
+      }
+    }
+  }
+  ngOnInit() {
+    if (this.ideaId) {
+      this.editFlag.set(true)
+      this.idea$.subscribe(data => {
+        this.ideaForm.patchValue(data)
+        this.tags.clear()
+        data.tags.forEach(tag => this.tags.push(this.formBuilder.control(tag)))
       }, err => {
         console.log(err);
       })
-  }
-  ngOnInit() {
-    if (this.ideaId)
-      this.editFlag.set(true)
+    }
   }
 }
